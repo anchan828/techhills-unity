72458ea339bb7e66008569b03b566bab8c135911
Keigo Ando <anchan828@gmail.com>
2013-11-18 16:05:01 +0900

    Added Sprite - Performance2

---
+++ Assets/Sprite - Performance2/Init2.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init2 : MonoBehaviour
+{
+		float time;
+		int count, frame, fps;
+		bool init = false;
+		public GameObject spaceShipDefault, spaceShipOpacity;
+		private GameObject selectedSpaceShip;
+		void Start ()
+		{
+				Application.targetFrameRate = 60;
+		}
+
+		void Update ()
+		{
+
+				if (init && Input.touchCount > 0) {
+						Touch[] touches = Input.touches;
+
+						foreach (var touch in touches) {
+								TouchPhase touchPhase = touch.phase;
+								switch (touchPhase) {
+								case TouchPhase.Began:
+								case TouchPhase.Moved:
+										Vector3 screenToWorldPoint = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 0));
+										Instantiate (selectedSpaceShip, new Vector2 (screenToWorldPoint.x, screenToWorldPoint.y), Quaternion.identity);
+										count++;
+										break;
+								case TouchPhase.Stationary:
+								case TouchPhase.Ended:
+								case TouchPhase.Canceled:
+								default:
+										break;
+								}
+						}
+				}
+
+				if (1 <= time) {
+						fps = frame;
+						time = 0;
+						frame = 0;
+				}
+				frame++;
+				time += Time.deltaTime;
+		}
+
+		void DrawButton (string text, GameObject spaceShip)
+		{
+				if (GUILayout.Button (text, GUILayout.Width (128), GUILayout.Height (128))) {
+						selectedSpaceShip = spaceShip;
+						init = true;
+				}
+		}
+
+		void OnGUI ()
+		{
+				GUI.skin.box.richText = true;
+				if (!init) {
+						DrawButton ("SpriteDefault", spaceShipDefault);
+						DrawButton ("SpriteOpacity", spaceShipOpacity);
+				} else {
+						GUILayout.Box ("<size=30>" + fps + " fps</size>");
+						GUILayout.Box ("<size=30>" + count.ToString () + "</size>");
+				}
+		}
+}
